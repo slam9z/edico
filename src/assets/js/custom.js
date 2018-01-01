fd35d5b56ff2e0d112787baa9741a9aa769cc341
Connie Lam <lam_9@hotmail.com>
2018-01-01 11:47:28 -0500

    Country fixed

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -255,57 +255,57 @@
     });
 
     //progress bar
-    var x = setInterval(function() {
-    // utility methods
+    if ($('#progress-solid-line').is(":visible") === 'true') {
+        var x = setInterval(function() {
+                // utility methods
 
-    const writeValue = (elementId, value) => document.getElementById(elementId).textContent = value;
-    const weiToEther = wei => wei / 10**18;
-    const totalEther = 750000000/8500;
-    const percent = ether => (ether / totalEther * 100).toFixed(1);
+                const writeValue = (elementId, value) => document.getElementById(elementId).textContent = value;
+                const weiToEther = wei => wei / 10 ** 18;
+                const totalEther = 750000000 / 8500;
+                const percent = ether => (ether / totalEther * 100).toFixed(1);
 
-    //const targetApi = 'https://ropsten.infura.io/Kgx1nx3BuoZLPDNH2RkK';
-    const targetApi = 'https://mainnet.infura.io/Kgx1nx3BuoZLPDNH2RkK';
-    const contractAddress = '0xCe53a179047ebed80261689367c093C90A94cC08';
-    //'0x01760d015473A4Fd33466F00f9A94405376565FD';
+                //const targetApi = 'https://ropsten.infura.io/Kgx1nx3BuoZLPDNH2RkK';
+                const targetApi = 'https://mainnet.infura.io/Kgx1nx3BuoZLPDNH2RkK';
+                const contractAddress = '0xCe53a179047ebed80261689367c093C90A94cC08';
+                //'0x01760d015473A4Fd33466F00f9A94405376565FD';
 
-    ///// getting contract
-    const web3 = new Web3(new Web3.providers.HttpProvider(targetApi));
-    const totalRaised = web3.fromWei(web3.eth.getBalance(contractAddress),"ether");
+                ///// getting contract
+                const web3 = new Web3(new Web3.providers.HttpProvider(targetApi));
+                const totalRaised = web3.fromWei(web3.eth.getBalance(contractAddress), "ether");
 
-    /// read and display values
-    //writeValue('contractAddress', contractAddress);
-    writeValue('totalRaised', 'Raised: ' + percent(totalRaised) + '%');
-    let etherPercent = percent(totalRaised) + '%';
-    //writeValue('totalRaisedPercent', etherPercent);
+                /// read and display values
+                //writeValue('contractAddress', contractAddress);
+                writeValue('totalRaised', 'Raised: ' + percent(totalRaised) + '%');
+                let etherPercent = percent(totalRaised) + '%';
+                //writeValue('totalRaisedPercent', etherPercent);
 
-    $('#progress-solid-line').css('width', etherPercent);
-    //$('#progress').text((progress * 100).toFixed(1) + '%').css('left', progress * 338 - 30 + 'px');
-    //$('#eth-amount').text(res.eth);
-    },
-    1000);
+                $('#progress-solid-line').css('width', etherPercent);
+                //$('#progress').text((progress * 100).toFixed(1) + '%').css('left', progress * 338 - 30 + 'px');
+                //$('#eth-amount').text(res.eth);
+            },
+            1000);
+    }
 
 
-  //block
-  $("#tokenBtn").click(function(event){
-      $.ajax({
-        url: "https://ipapi.co/json",
-        type: 'GET',
-        success: function(json)
-        {
-           console.log("My country is: " + json.country);
-           if(json.country == "US1") {
-              alert("We detect your IP belongs to US citizen or resident, whose legislation conflicts with joining the current sales event. We apologize for the inconvenience ");
-           } else {
-//              this.router.navigate(['register']);
-                window.open("/register","_self")
-           }
-        },
-        error: function(err)
-        {
-         console.log("Request failed, error= " + err);
-        }
-      });
-  })
+
+    //block
+    $("#tokenBtn").click(function(event) {
+        $.ajax({
+            url: "https://ipapi.co/json",
+            type: 'GET',
+            success: function(json) {
+                // console.log("My country is: " + json.country);
+                if (json.country == "US") {
+                    alert("We detect your IP belongs to US citizen or resident, whose legislation conflicts with joining the current sales event. We apologize for the inconvenience ");
+                } else {
+                    window.open("/register", "_self");
+                }
+            },
+            error: function(err) {
+                console.error("Request failed, error= " + err);
+            }
+        });
+    })
 
 
 })(jQuery);
